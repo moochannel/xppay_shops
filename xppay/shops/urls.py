ac92bf8b39cbac8be298959f105206bcc015294c
moochannel <moochannel@gmail.com>
2018-02-06 17:23:31 +0900

    URL関連の調整
    - faviconを設置します
    - shops名前空間を明記し、url関数の呼び出しで名前空間を指定します
    - ホスト名だけで参照されたら/shops/にリダイレクトされるようにします

--- xppay/shops/urls.py
+++ xppay/shops/urls.py
@@ -5,17 +5,18 @@ from .views import (
     ShopDetail, ShopList, ShopUpdate
 )
 
+app_name = 'shops'
 urlpatterns = [
-    path('', ShopList.as_view(), name='index'),
+    path('', ShopList.as_view(), name='shop_list'),
     path('<int:pk>/', ShopDetail.as_view(), name='shop_detail'),
     path('add/', ShopCreate.as_view(), name='shop_add'),
     path('<int:pk>/edit/', ShopUpdate.as_view(), name='shop_edit'),
-    path('<int:shop_id>/benefits/', BenefitList.as_view(), name='shop_benefit_list'),
-    path('<int:shop_id>/benefits/add/', BenefitCreate.as_view(), name='shop_benefit_add'),
+    path('<int:shop_id>/benefits/', BenefitList.as_view(), name='benefit_list'),
+    path('<int:shop_id>/benefits/add/', BenefitCreate.as_view(), name='benefit_add'),
     path(
-        '<int:shop_id>/benefits/<int:pk>/edit/', BenefitUpdate.as_view(), name='shop_benefit_edit'
+        '<int:shop_id>/benefits/<int:pk>/edit/', BenefitUpdate.as_view(), name='benefit_edit'
     ),
-    path('<int:shop_id>/photos/', PhotoList.as_view(), name='shop_photo_list'),
-    path('<int:shop_id>/photos/add/', PhotoCreate.as_view(), name='shop_photo_add'),
-    path('<int:shop_id>/photos/<int:pk>/del/', PhotoDelete.as_view(), name='shop_photo_del'),
+    path('<int:shop_id>/photos/', PhotoList.as_view(), name='photo_list'),
+    path('<int:shop_id>/photos/add/', PhotoCreate.as_view(), name='photo_add'),
+    path('<int:shop_id>/photos/<int:pk>/del/', PhotoDelete.as_view(), name='photo_del'),
 ]
--- xppay/xppay/urls.py
+++ xppay/xppay/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/shops/')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('shops/', include('shops.urls')),
