a89e69110646d8d7de305cd43cbf196271576cad
moochannel <moochannel@gmail.com>
2018-02-17 14:05:20 +0900

    labo.xppay.jp用の設定を追加します

--- xppay/xppay/settings_production.py
+++ xppay/xppay/settings_production.py
@@ -6,3 +6,6 @@ STATIC_ROOT = '/usr/src/static/'
 MEDIA_ROOT = '/usr/src/media/'
 INSTALLED_APPS.remove('debug_toolbar')      # noqa
 MIDDLEWARE.remove('debug_toolbar.middleware.DebugToolbarMiddleware')    # noqa
+# SOCIAL_AUTH_DISCORD_KEY = ''
+# SOCIAL_AUTH_DISCORD_SECRET = ''
+# SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
